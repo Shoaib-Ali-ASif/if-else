90c8517b59e177337f0974469b6930e607961cf7
shoaib-ali-asif <eagleshebi@gmail.com>
2023-05-27 15:06:32 +0500

    changed multiple things

---
+++ js/app.js
@@ -0,0 +1,23 @@
+// Check two given numbers and return true if one of the numbers is 50 or if their sum is 50. 
+let numOne = 20;
+let numTwo = 50;
+if(numOne == numTwo){
+    console.log('equal');
+}else{
+    console.log('notEqual');
+}
+
+// Check from the given integer, whether it is positive or negative. 
+let one = -1;
+let two = 1;
+if(one == two){
+    console.log('postive')
+}else{
+    console.log('negative');
+}
+
+
+
+// Check whether a given number is even or odd. 
+// Check whether a given positive number is a multiple of 3. 
+// Determine whether a given year is a leap year.
