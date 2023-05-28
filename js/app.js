d2c68ecae95d3079542e7dfb4272ff06ab833c22
shoaib-ali-asif <eagleshebi@gmail.com>
2023-05-28 17:52:58 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Check two given numbers and return true if one of the numbers is 50 or if their sum is 50. 
-let numOne = 20;
-let numTwo = 50;
-if(numOne == numTwo){
-    console.log('equal');
+let numOne = 10;
+let numTwo = 20;
+if(numOne == 50 || numTwo == 50 || numOne + numTwo == 50){
+    console.log('true');
 }else{
-    console.log('notEqual');
+    console.log('false');
 }
+
+
+
 
 // Check from the given integer, whether it is positive or negative. 
 let one = 1;
@@ -18,5 +21,28 @@ if(one >= 0){
 
 
 // Check whether a given number is even or odd. 
+let number = 22 ;
+if(number % 2 == 0){
+    console.log('even');
+}else{
+    console.log('odd');
+}
 // Check whether a given positive number is a multiple of 3. 
+let Number = 55 ;
+if(Number % 3 == 0){
+    console.log('multipleOf3');
+}else{
+   console.log('notMultipleOf3'); 
+}
 // Determine whether a given year is a leap year.
+
+// Any year that is evenly divisible by 4 is a leap year.
+
+let year = 2023;
+if( year % 4 == 0){
+    console.log('leapYear');
+}else{
+    console.log('notLeapYear');
+}
+
+
